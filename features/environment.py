83a2da0f0a9c06dc281795132b2db964cc5e0740
umme-ha <humme005@gmail.com>
2026-06-27 19:48:32 +0530

    comment

--- features/environment.py
+++ features/environment.py
@@ -27,3 +27,4 @@ def after_scenario(context, scenario):
         context.driver.save_screenshot(f"reports/screenshots/{screenshot_name}.png")
     context.driver.delete_all_cookies()
     context.driver.quit()
+    # quit
