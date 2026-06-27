b3c4e42258d62a69eb416a2151d61983a52f6d56
umme-ha <humme005@gmail.com>
2026-06-27 20:24:47 +0530

    allure reports in CI

--- features/environment.py
+++ features/environment.py
@@ -1,5 +1,5 @@
 import os
-
+import allure
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
@@ -27,6 +27,15 @@ def after_scenario(context, scenario):
     if scenario.status == "failed":
         os.makedirs("reports/screenshots", exist_ok=True)
         screenshot_name = scenario.name.replace(" ", "_").replace("/", "_")
-        context.driver.save_screenshot(f"reports/screenshots/{screenshot_name}.png")
+        screenshot_path = f"reports/screenshots/{screenshot_name}.png"
+        context.driver.save_screenshot(screenshot_path)
+
+
+        with open(screenshot_path, "rb") as image_file:
+            allure.attach(
+                image_file.read(),
+                name=screenshot_name,
+                attachment_type=allure.attachment_type.PNG
+            )
     context.driver.delete_all_cookies()
     context.driver.quit()
